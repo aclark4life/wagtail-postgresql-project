12624c421ee6add21f951aca295373a7a06e22ef
Jeffrey A. Clark <aclark@aclark.net>
2024-11-30 17:11:29 -0500

    Remove apps

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -24,8 +24,6 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # Application definition
 
 INSTALLED_APPS = [
-    "apps.home",
-    "apps.search",
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
     "wagtail.embeds",
