265283745f5c868c8e18d4a4a14094b728de37b8
Jeffrey A. Clark <aclark@aclark.net>
2024-11-30 17:12:45 -0500

    Import dj database url

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/
 """
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
