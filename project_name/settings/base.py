eae0dd3cce1d2db69957496e2629f87b6036430e
Jeffrey A. Clark <aclark@aclark.net>
2024-11-29 19:59:06 -0500

    Use postgresql

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -84,13 +84,8 @@ WSGI_APPLICATION = "{{ project_name }}.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-
+DATABASES = {}
+DATABASES["default"] = dj_database_url.parse("postgres://localhost/{{ project_name }}")
 
 # Password validation
 # https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#auth-password-validators
